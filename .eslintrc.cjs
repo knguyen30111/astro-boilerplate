952d8ff06212e08a797907a989413e84dd070bc8
Nguyen Le <knguyen30111@gmail.com>
2023-11-26 14:43:42 +0700

    handle eslint + prettierrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Enable this plugin
+      plugins: ["astro"],
+      env: {
+        // Enables global variables available in Astro components.
+        node: true,
+        "astro/astro": true,
+        es2020: true,
+      },
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        // The script of Astro components uses ESM.
+        sourceType: "module",
+      },
+      rules: {
+        // Enable recommended rules
+        "astro/no-conflict-set-directives": "error",
+        "astro/no-unused-define-vars-in-style": "error",
+        "@typescript-eslint/no-explicit-any": "warn",
+
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
